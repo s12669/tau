717a2ddfd7bee5acdf8b59f784ca4177e54fd611
s12669 <maciej2106@gmail.com>
2017-03-07 10:40:00 +0100

    lab2

---
+++ lab2/src/main/java/pl/edu/pjwstk/lab2/IVector.java
@@ -0,0 +1,9 @@
+package pl.edu.pjwstk.lab2;
+
+import java.util.Vector;
+
+public interface IVector {
+    Vector<Integer> add (Vector<Integer> v);
+
+    //Vector<Integer> addVectors(Vector<Integer> v, Vector<Integer> vec);
+}
