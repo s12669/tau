95191fe474f16c68febd8eac93ebad81abc0ae7e
s12669 <maciej2106@gmail.com>
2017-03-13 10:59:00 +0100

    repair3

---
+++ lab2/src/main/java/pl/edu/pjwstk/IVector.java
@@ -0,0 +1,9 @@
+package pl.edu.pjwstk;
+
+import java.util.Vector;
+
+public interface IVector {
+    Vector<Integer> add (Vector<Integer> v);
+
+    //Vector<Integer> addVectors(Vector<Integer> v, Vector<Integer> vec);
+}
---
+++ lab2/src/test/java/pl/edu/pjwstk/VectorTest.java
@@ -0,0 +1,37 @@
+package pl.edu.pjwstk.lab2;
+
+import org.junit.Test;
+import pl.edu.pjwstk.VectorImpl;
+
+import java.util.Vector;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class VectorTest {
+
+    private VectorImpl vector = new VectorImpl();
+
+    @Test
+    public void testEmpty() {
+        assertTrue(vector.getVector().isEmpty());
+    }
+
+    @Test
+    public void testVectors() {
+        Vector<Integer> vector1 = new Vector<>(2);
+        vector1.add(1);
+        vector1.add(5);
+        vector.setVector(vector1);
+       assertFalse(vector.getVector().isEmpty());
+    }
+
+    @Test
+    public void adding(){
+        Vector<Integer> vector1 = new Vector<>(2);
+        vector1.add(1);
+        Vector<Integer> vector2 = new Vector<>(2);
+        vector2.add(5);
+        assertTrue(vector1.size()==vector2.size());
+    }
+}
